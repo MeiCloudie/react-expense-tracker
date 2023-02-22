f58a5b6eee33388ff7bdd070e99608dbe6c55b24
Thuc Van Truong <truongthucvan1242@gmail.com>
2023-02-22 21:48:57 +0700

    state and useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,12 +1,18 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card'
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  let title = props.title;
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluated by React');
+  
+  //let title = props.title;
   
   const clickHandler = () => {
-    title = 'Updated!';
+    //title = 'Updated!';
+    setTitle('Updated!');
     console.log(title);
   };
   
